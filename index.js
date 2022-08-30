b9d686cdf5585f3d2f6b22fbec5ed03d2f18097c
DRACOX2500 <alexis.forestier2001@gmail.com>
2022-08-30 15:24:04 +0200

    init

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config({path: 'token.env'});
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({
+    intents: [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMessages,
+    ]
+  });
+const token = process.env.DISCORD_TOKEN;
+
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on("message", msg => {
+    if (msg.content === "ping") {
+        msg.reply("pong");
+    }
+})
+
+client.login(token);
